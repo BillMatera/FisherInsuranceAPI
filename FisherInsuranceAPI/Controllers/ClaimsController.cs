d997908f7679967044534c1967f32a7a69293b4c
BillMatera <matera.3@osu.edu>
2017-02-06 21:59:20 -0500

    Lab2 API
    Lab2 API

---
+++ FisherInsuranceAPI/Controllers/ClaimsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FisherInsuranceAPI.Controllers
+{
+    [Route("api/customer/claims")]
+    public class ClaimsController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/customer/claims/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("The id is: " + id);
+        }
+
+        // POST api/customer/claims
+        [HttpPost]
+        public IActionResult Post([FromBody]string value)
+        {
+            return Created("", value);
+        }
+
+        // PUT api/customer/claims/id
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value)
+        {
+            return NoContent();
+        }
+
+        // DELETE  api/customer/claims/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Delete(id);
+        }
+    }
+}
